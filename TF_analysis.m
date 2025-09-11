f18a334c8a8ab7db638932099ede5167cffd527f
thouardi <165410158+thouardi@users.noreply.github.com>
2025-09-11 17:48:13 +0200

    Update TF_analysis.m

--- TF_analysis.m
+++ TF_analysis.m
@@ -1,9 +1,9 @@
 %% === Load the cleaned EEG dataset ===
-EEG = pop_loadset('/Users/iristhouard/Documents/MATLAB/codes/P300/Preprocessing_Pipeline/EEG_afterICA/propre.set'); % load cleaned EEG
+EEG = pop_loadset('/Users/iristhouard/Documents/MATLAB/codes/P300/EEG_afterICA/propre.set');
 
 %% === Define parameters ===
-epoch_time = [-0.2 1];           % epoch window in seconds
-baseline    = [-200 0];          % baseline in ms
+epoch_time = [-0.2 1];               % epoch window in seconds
+baseline    = [-200 0];              % baseline in ms (only for pop_rmbase)
 electrodes  = {'Pz','P3','P4','Cz'}; % electrodes of interest
 save_figs   = true;
 outdir = fullfile(pwd,'TF_results');
@@ -19,82 +19,99 @@ EEG_std = pop_rmbase(EEG_std, baseline);
 %% === Parameters for TF analysis ===
 fs      = EEG.srate;             % sampling rate
 times   = EEG_odd.times;         % ms
-freqs   = 1:1:40;                % frequency range for STFT
+freqs   = 1:1:40;                % frequency range for STFTfi([], 1, 16)
 
 % STFT parameters
 win_len = round(0.2*fs);         % 200 ms window
 overlap = round(0.9*win_len);    % 90% overlap
 
-%% === Compute Time-Frequency using STFT ===
+%% === Time-Frequency analysis with STFT (linear scale, no baseline normalization) ===
 for i = 1:length(electrodes)
     chanIdx = find(strcmpi({EEG.chanlocs.labels}, electrodes{i}));
     if isempty(chanIdx), continue; end
-    
-    % Extract data (trials averaged)
-    sig_odd = mean(squeeze(EEG_odd.data(chanIdx,:,:)),2); % average across trials
+
+    % Extract data (average across trials)
+    sig_odd = mean(squeeze(EEG_odd.data(chanIdx,:,:)),2);
     sig_std = mean(squeeze(EEG_std.data(chanIdx,:,:)),2);
-    
-    % STFT (spectrogram)
+
+    % Compute spectrogram (STFT)
     [~,F,T,Podd] = spectrogram(sig_odd,win_len,overlap,freqs,fs,'yaxis');
     [~,~,~,Pstd] = spectrogram(sig_std,win_len,overlap,freqs,fs,'yaxis');
-    
+
     % Convert time axis T (s) into ms relative to epoch start
     Tms = (T + epoch_time(1)) * 1000;
-    
-    % Plot
+
+    % Plot raw power spectrograms
     figure('Color','w','Name',['STFT - ' electrodes{i}]);
     subplot(1,2,1);
-    imagesc(Tms,F,10*log10(Pstd)); axis xy;
-    xlabel('Time (ms)'); ylabel('Freq (Hz)');
+    imagesc(Tms,F,Pstd); axis xy;
+    xlabel('Time (ms)'); ylabel('Frequency (Hz)');
     title(['Standard - ' electrodes{i}]); colorbar;
     xlim([-200 1000]); ylim([0 40]);
-    
+
     subplot(1,2,2);
-    imagesc(Tms,F,10*log10(Podd)); axis xy;
-    xlabel('Time (ms)'); ylabel('Freq (Hz)');
+    imagesc(Tms,F,Podd); axis xy;
+    xlabel('Time (ms)'); ylabel('Frequency (Hz)');
     title(['Oddball - ' electrodes{i}]); colorbar;
     xlim([-200 1000]); ylim([0 40]);
-    
+
     if save_figs
-        saveas(gcf, fullfile(outdir, sprintf('STFT_%s.png', electrodes{i})));
+        saveas(gcf, fullfile(outdir, sprintf('STFT_%s_raw.png', electrodes{i})));
     end
 end
 
-%% === Compute Time-Frequency using Wavelet Transform (amor) ===
+
+%% === Time-Frequency analysis with Wavelet Transform (linear scale + cone of influence, raw power) ===
 for i = 1:length(electrodes)
     chanIdx = find(strcmpi({EEG.chanlocs.labels}, electrodes{i}));
     if isempty(chanIdx), continue; end
-    
+
     % Extract averaged signals
     sig_odd = mean(squeeze(EEG_odd.data(chanIdx,:,:)),2);
     sig_std = mean(squeeze(EEG_std.data(chanIdx,:,:)),2);
-    
-    % Continuous wavelet transform with Morlet
-    [cfs_odd,Fodd] = cwt(sig_odd,fs,'amor');
-    [cfs_std,Fstd] = cwt(sig_std,fs,'amor');
-    
-    % Plot
+
+    % Compute Continuous Wavelet Transform (CWT) with Morlet
+    [cfs_odd,Fodd,coi_odd] = cwt(sig_odd,fs,'amor'); % Oddball
+    [cfs_std,Fstd,coi_std] = cwt(sig_std,fs,'amor'); % Standard
+
+    % Convert to power
+    pow_odd = abs(cfs_odd).^2;
+    pow_std = abs(cfs_std).^2;
+
+    % Convert COI to ms relative to epoch start
+    coi_odd_ms = (coi_odd - 1)/fs*1000 + epoch_time(1)*1000;
+    coi_std_ms = (coi_std - 1)/fs*1000 + epoch_time(1)*1000;
+
+    % Plot Standard condition
     figure('Color','w','Name',['Wavelet - ' electrodes{i}]);
     subplot(1,2,1);
-    surface(times,Fstd,abs(cfs_std)); shading interp; axis tight; set(gca,'YScale','log');
-    xlabel('Time (ms)'); ylabel('Freq (Hz)');
+    surface(times,Fstd,pow_std); shading interp; axis tight;
+    set(gca,'YScale','linear'); % linear frequency scale
+    xlabel('Time (ms)'); ylabel('Frequency (Hz)');
     title(['Standard - ' electrodes{i}]); colorbar;
     xlim([-200 700]); ylim([0 35]);
-    
+    hold on;
+    % Plot COI as a white dashed line at max frequency
+    plot(coi_std_ms, Fstd(end)*ones(size(coi_std_ms)),'w--','LineWidth',2);
+
+    % Plot Oddball condition
     subplot(1,2,2);
-    surface(times,Fodd,abs(cfs_odd)); shading interp; axis tight; set(gca,'YScale','log');
-    xlabel('Time (ms)'); ylabel('Freq (Hz)');
+    surface(times,Fodd,pow_odd); shading interp; axis tight;
+    set(gca,'YScale','linear'); % linear frequency scale
+    xlabel('Time (ms)'); ylabel('Frequency (Hz)');
     title(['Oddball - ' electrodes{i}]); colorbar;
     xlim([-200 700]); ylim([0 35]);
-    
+    hold on;
+    % Plot COI as a white dashed line at max frequency
+    plot(coi_odd_ms, Fodd(end)*ones(size(coi_odd_ms)),'w--','LineWidth',2);
+
     if save_figs
-        saveas(gcf, fullfile(outdir, sprintf('Wavelet_%s.png', electrodes{i})));
+        saveas(gcf, fullfile(outdir, sprintf('Wavelet_%s_raw_coi.png', electrodes{i})));
     end
 end
 
-%% === Group-level Topography of TF differences ===
-% Example: average Odd-Std power in theta (4-7 Hz) and P300 window (300–450 ms)
 
+%% === Group-level Topography of TF differences (theta/alpha band in P300 window, raw power) ===
 freq_band = [4 12]; time_win = [200 450];
 [~,t1] = min(abs(times-time_win(1))); [~,t2] = min(abs(times-time_win(2)));
 
@@ -105,10 +122,14 @@ for ch=1:EEG.nbchan
     [cfs_odd,F] = cwt(sig_odd,fs,'amor');
     [cfs_std,~] = cwt(sig_std,fs,'amor');
 
+    pow_odd = abs(cfs_odd).^2;
+    pow_std = abs(cfs_std).^2;
+
+    % Extract power in freq/time band of interest
     idxF = find(F>=freq_band(1) & F<=freq_band(2));
-    pow_odd = mean(abs(cfs_odd(idxF,t1:t2)),[1 2]);
-    pow_std = mean(abs(cfs_std(idxF,t1:t2)),[1 2]);
-    theta_diff(ch) = pow_odd - pow_std;
+    pow_band_odd = mean(pow_odd(idxF,t1:t2),[1 2]);
+    pow_band_std = mean(pow_std(idxF,t1:t2),[1 2]);
+    theta_diff(ch) = pow_band_odd - pow_band_std;
 end
 
 figure('Color','w');
